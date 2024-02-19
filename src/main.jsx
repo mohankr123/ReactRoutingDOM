3a2d790bf677c0b590f0861edb1823fa324b5104
Mohan Kumar <mohankr2006@gmail.com>
2024-02-19 18:59:40 +0530

    route setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root, {loader as rootLoader } from './routes/root.jsx'
+import ErrorPage from './error-page.jsx'
+import Contact from './routes/contact.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    loader:rootLoader,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </React.StrictMode>,
 )
